57e58e4eeb96450d2b3dfcc9dbf16dfccbe93cfd
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-08-01 01:12:24 +0300

    refactor: refactor components as class

--- src/components/Todo-list-item/Todo-list-item.jsx
+++ src/components/Todo-list-item/Todo-list-item.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './Todo-list-item.scss';
 
-const TodoListItem = ({ item }) => {
-  const { label } = item;
-  const classes = [];
-  if (item.important) {
-    classes.push('active');
-  }
-  return (
-    <div className="todo-list-item">
-      <span className={classes.join(' ')}>{label}</span>
-      <div className="control-buttons">
-        <button type="button" className="btn btn-outline-danger control-buttons__item">
-          <i className="fa fa-trash-o" />
-        </button>
-        <button type="button" className="btn btn-outline-success control-buttons__item">
-          <i className="fa fa-exclamation" />
-        </button>
+export default class TodoListItem extends Component {
+  render() {
+    const { item } = this.props;
+    const classes = [];
+    if (item.important) {
+      classes.push('active');
+    }
+    return (
+      <div className="todo-list-item">
+        <span className={classes.join(' ')}>{item.label}</span>
+        <div className="control-buttons">
+          <button type="button" className="btn btn-outline-danger control-buttons__item">
+            <i className="fa fa-trash-o" />
+          </button>
+          <button type="button" className="btn btn-outline-success control-buttons__item">
+            <i className="fa fa-exclamation" />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 TodoListItem.propTypes = {
   item: PropTypes.shape({
@@ -30,5 +32,3 @@ TodoListItem.propTypes = {
     important: PropTypes.bool.isRequired,
   }).isRequired,
 };
-
-export default TodoListItem;
